fde378c97307d815e7de400e93d7e3e16f2a639f
carbaviana-arch <carbaviana@gmail.com>
2025-11-20 23:29:40 +0100

    Crear inventario.js

---
+++ inventario.js
@@ -0,0 +1,7 @@
+// Lógica base para el módulo de Inventario
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Módulo Inventario cargado correctamente");
+
+  // Aquí añadiremos las funciones de gestión del inventario
+});
