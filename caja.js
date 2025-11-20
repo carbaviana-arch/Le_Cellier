358402c2f9912850039652ffa82383d4691793c7
carbaviana-arch <carbaviana@gmail.com>
2025-11-20 23:41:10 +0100

    Crear caja.js

---
+++ caja.js
@@ -0,0 +1,7 @@
+// Lógica base para el módulo de Caja
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Módulo Caja cargado correctamente");
+
+  // Aquí se añadirán funciones para gestionar cierres de caja diarios
+});
