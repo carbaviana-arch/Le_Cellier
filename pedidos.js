dca4221b5734798c9bb4d09d4a53755a88ac6139
carbaviana-arch <carbaviana@gmail.com>
2025-11-20 23:34:34 +0100

    Crear pedidos.js

---
+++ pedidos.js
@@ -0,0 +1,7 @@
+// Lógica base para el módulo de Pedidos
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Módulo Pedidos cargado correctamente");
+
+  // Aquí añadiremos las funciones para gestionar pedidos de bebidas y licores
+});
