18689d9adcc873268fb61b4a3440a82d7f997e75
carbaviana-arch <carbaviana@gmail.com>
2025-11-20 23:39:05 +0100

    Crear eventos.js

---
+++ eventos.js
@@ -0,0 +1,7 @@
+// Lógica base para el módulo de Eventos
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Módulo Eventos cargado correctamente");
+
+  // Aquí se añadirán funciones para gestionar eventos y calendario
+});
